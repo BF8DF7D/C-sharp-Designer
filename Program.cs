b4df368b42b4833cb483de8a48ef13888a41f664
Sus <nikitosik0202@yandex.ru>
2022-01-09 23:50:58 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace С_sharp_Designer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
